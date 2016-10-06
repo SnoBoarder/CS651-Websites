7dfabf9536e7c11c7b11fd4c77753ee56d9549ed
Brian Tran <bttran89@gmail.com>
2016-10-05 21:46:17 -0400

    HW4 part 1 - update counting words system with ajax calls

---
+++ HW4/BtranCountingWordsWebsite/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="css/main.css" rel="stylesheet" type="text/css" />
+    <link href="css/max-width-800px.css" rel="stylesheet" type="text/css" />
+    <link href="css/max-width-490px.css" rel="stylesheet" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <script type="text/javascript">
+        function ReceiveServerData(arg, context) {
+            document.getElementById("output").value = arg;
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="container">
+            <div id="header">
+                <h1>Brian Tran's Word Counting</h1>
+            </div>
+
+            <div id="content">
+                <h1>Input</h1>
+                <asp:TextBox ID="input" CssClass="input" TextMode="MultiLine" Wrap="true" runat="server" onkeyup="CallServer(document.getElementById('input').value);">Tell your story here.</asp:TextBox>
+            </div>
+
+            <div id="contentb">
+                <h1>Output</h1>
+                <asp:TextBox ID="output" CssClass="output" TextMode="MultiLine" Wrap="true" ReadOnly="true" runat="server">See your word counts here.</asp:TextBox>
+            </div>
+
+            <div id="footer">
+                <p>Copyright Brian Tran</p>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ HW4/BtranCountingWordsWebsite/Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web.UI;
+
+public partial class _Default : System.Web.UI.Page, System.Web.UI.ICallbackEventHandler
+{
+	private static StringBuilder _scratchStringBuilder;
+	private static Hashtable _scratchHashtable;
+
+	protected string _wordCountResults;
+
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		_scratchStringBuilder = new StringBuilder();
+		_scratchHashtable = new Hashtable();
+
+		ClientScriptManager csm = Page.ClientScript;
+
+		string cbReference = csm.GetCallbackEventReference(this, "arg", "ReceiveServerData", "");
+
+		// registers function to call RaiseCallbackEvent
+		string callbackScript = "function CallServer(arg) {" + cbReference + "; }";
+		csm.RegisterClientScriptBlock(this.GetType(), "CallServer", callbackScript, true);
+	}
+
+	public string GetCallbackResult()
+	{
+		return _wordCountResults;
+	}
+
+	public void RaiseCallbackEvent(string eventArgument)
+	{
+		CountWords(_scratchHashtable, eventArgument);
+		_wordCountResults = GenerateOutput(_scratchHashtable);
+	}
+
+	private static void CountWords(Hashtable hashTable, string words)
+	{
+		// clear scratch hashtable
+		hashTable.Clear();
+
+		// strip out all special characters from the string
+		string cleanWords = Regex.Replace(words, "[^0-9a-zA-Z ]+", "");
+
+		string[] wordArray = cleanWords.Split(' ');
+
+		string word;
+		for (int i = 0; i < wordArray.Length; ++i)
+		{
+			word = wordArray[i];
+
+			if (word.Length > 1)
+				word = word.ToLower();
+
+			if (!hashTable.ContainsKey(word))
+			{
+				hashTable[word] = 1;
+			}
+			else
+			{
+				hashTable[word] = (int)hashTable[word] + 1;
+			}
+		}
+	}
+
+	private static string GenerateOutput(Hashtable hashTable)
+	{
+		_scratchStringBuilder.Clear();
+
+		foreach (DictionaryEntry e in hashTable)
+		{
+			_scratchStringBuilder.Append(e.Key);
+			_scratchStringBuilder.Append(" : ");
+			_scratchStringBuilder.Append(e.Value);
+			_scratchStringBuilder.Append("\n");
+		}
+
+		return _scratchStringBuilder.ToString();
+	}
+}
