f322d3b6dcc5300d9c2301dc509a608fab5c543c
Brian Tran <bttran89@gmail.com>
2016-12-10 13:29:19 -0500

    Adding project template ideas

---
+++ FinalProject/TranMini/TranMini/GameServer/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TranMini.GameServer
+{
+    public class Game
+    {
+        private readonly static Lazy<Game> _instance = new Lazy<Game>(() => new Game());
+
+        public static Game Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        //public UserHandler UserHandler { get; private set; }
+
+        private Game()
+        {
+            // private constructor since this should be a singleton
+
+        }
+    }
+}
---
+++ FinalProject/TranMini/TranMini/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TranMini.Startup.Startup))]
+
+namespace TranMini.Startup
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
